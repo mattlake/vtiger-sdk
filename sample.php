01253ebfcf4a10795bdc6a37a10b8b2115801f74
matthew.lake <matthew.lake@9goup.co.uk>
2021-02-23 15:16:26 +0000

    Add Symfony HTTP Client

--- sample.php
+++ sample.php
@@ -6,5 +6,8 @@ require_once './vendor/autoload.php';
 
 use Trunk\VtigerApi\VtigerApi;
 
+// Create Dependency Injection Container
+$container = new DI\Container();
+
 // Create APi Instance
 $api = VtigerApi::url('https://yoururlhere')->authenticate(config::USERNAME, config::SECRET);
