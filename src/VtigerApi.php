0d6853fef7aa40d26cb0fc441d90accd4fd12459
matthew.lake <matthew.lake@9goup.co.uk>
2021-02-24 14:57:41 +0000

    Complete Auth Path

--- src/VtigerApi.php
+++ src/VtigerApi.php
@@ -6,7 +6,7 @@ class VtigerApi
 {
     private static $instance;
     private $url;
-    private $accessToken;
+    private $sessionId;
     private $client;
 
     private function __construct($client)
@@ -41,8 +41,7 @@ class VtigerApi
 
         $token = $this->makeChallenge($username);
         $hash = $this->makeKey($token, $secret);
-
-        echo $hash . PHP_EOL;
+        $this->sessionId = $this->getSessionId($username, $hash);
         return $this;
     }
 
@@ -59,8 +58,42 @@ class VtigerApi
         return $content['result']['token'];
     }
 
-    private function makeKey(string $token, string $secret):string
+    private function makeKey(string $token, string $secret): string
     {
-        return md5($token.$secret);
+        return md5($token . $secret);
+    }
+
+    private function getSessionId(string $username, string $accessKey): string
+    {
+        $data = [
+            'operation' => 'login',
+            'username' => $username,
+            'accessKey' => $accessKey
+        ];
+
+        $body = $this->client->createStream($this->convertArrayParamsToString($data));
+
+        $request = $this->client->createRequest('POST', $this->url . '/webservice.php')
+            ->withHeader('Content-Type', 'application/x-www-form-urlencoded')
+            ->withBody($body);
+
+        $response = $this->client->sendRequest($request);
+
+        $content = json_decode($response->getBody()->getContents(), true);
+
+        if ($content['success'] == true) {
+            return $content['result']['sessionName'];
+        }
+
+        throw new \Exception($content['error']['code'] . ': ' . $content['error']['message']);
+    }
+
+    private function convertArrayParamsToString(array $params): string
+    {
+        $string = '';
+        foreach ($params as $key => $value) {
+            $string .= $key . '=' . $value . '&';
+        }
+        return $string;
     }
 }
