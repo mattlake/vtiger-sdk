197ee081e706b14b1309df8f2e25245018431e6d
matthew.lake <matthew.lake@9goup.co.uk>
2021-02-26 11:30:03 +0000

    Tidy tests

--- tests/VtigerApiTest.php
+++ tests/VtigerApiTest.php
@@ -4,19 +4,8 @@ require_once __DIR__ . '/../src/VtigerApi.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Trunk\VtigerSDK\VtigerApi;
-use DI\ContainerBuilder;
 
-beforeEach(function(){
-    // Create Dependency Injection Container
-    $containerBuilder = new ContainerBuilder();
-    $containerBuilder->addDefinitions(__DIR__ . '/../deps.php');
-    $this->container = $containerBuilder->build();
-});
 
-afterEach(function(){
-    unset($this->container);
-});
-
-it('can get instance', function(){
+it('can get instance using ::endpoint() method', function(){
     expect(VtigerApi::endpoint('https://www.endpoint.com'))->toBeInstanceof(VtigerApi::class);
 });
--- tests/VtigerRequestTest.php
+++ tests/VtigerRequestTest.php
@@ -1,54 +1,78 @@
 <?php
 
-require_once __DIR__.'/../src/VtigerApi.php';
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../src/VtigerApi.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-use Trunk\VtigerSDK\VtigerApi;
-use DI\ContainerBuilder;
+use Trunk\VtigerSDK\Http\VtigerRequest;
+use Trunk\VtigerSDK\Http\VtigerResponse;
 
-it('can create from constructor - GET', function(){
-    $testClass = new \Trunk\VtigerSDK\Http\VtigerRequest('GET');
-    expect($testClass)->toBeInstanceOf(\Trunk\VtigerSDK\Http\VtigerRequest::class);
-    expect($testClass->getRequestType())->toBe('GET');
-});
+it(
+    'can create from constructor - GET',
+    function () {
+        $testClass = new VtigerRequest('GET');
+        expect($testClass)->toBeInstanceOf(VtigerRequest::class);
+        expect($testClass->getRequestType())->toBe('GET');
+    }
+);
 
-it('can create from constructor - POST', function(){
-    $testClass = new \Trunk\VtigerSDK\Http\VtigerRequest('POSt');
-    expect($testClass)->toBeInstanceOf(\Trunk\VtigerSDK\Http\VtigerRequest::class);
-    expect($testClass->getRequestType())->toBe('POST');
-});
+it(
+    'can create from constructor - POST',
+    function () {
+        $testClass = new VtigerRequest('POSt');
+        expect($testClass)->toBeInstanceOf(VtigerRequest::class);
+        expect($testClass->getRequestType())->toBe('POST');
+    }
+);
 
-it('can create from constructor with mismatched case - GET', function(){
-    $testClass = new \Trunk\VtigerSDK\Http\VtigerRequest('gEt');
-    expect($testClass)->toBeInstanceOf(\Trunk\VtigerSDK\Http\VtigerRequest::class);
-    expect($testClass->getRequestType())->toBe('GET');
-});
+it(
+    'can create from constructor with mismatched case - GET',
+    function () {
+        $testClass = new VtigerRequest('gEt');
+        expect($testClass)->toBeInstanceOf(VtigerRequest::class);
+        expect($testClass->getRequestType())->toBe('GET');
+    }
+);
 
-it('can create from constructor with mismatched case - POST', function(){
-    $testClass = new \Trunk\VtigerSDK\Http\VtigerRequest('POst');
-    expect($testClass)->toBeInstanceOf(\Trunk\VtigerSDK\Http\VtigerRequest::class);
-    expect($testClass->getRequestType())->toBe('POST');
-});
+it(
+    'can create from constructor with mismatched case - POST',
+    function () {
+        $testClass = new VtigerRequest('POst');
+        expect($testClass)->toBeInstanceOf(VtigerRequest::class);
+        expect($testClass->getRequestType())->toBe('POST');
+    }
+);
 
-it('can be instantiated using ::get()', function(){
-    $testClass = \Trunk\VtigerSDK\Http\VtigerRequest::get();
-    expect($testClass)->toBeInstanceOf(\Trunk\VtigerSDK\Http\VtigerRequest::class);
-    expect($testClass->getRequestType())->toBe('GET');
-});
+it(
+    'can be instantiated using ::get()',
+    function () {
+        $testClass = VtigerRequest::get();
+        expect($testClass)->toBeInstanceOf(VtigerRequest::class);
+        expect($testClass->getRequestType())->toBe('GET');
+    }
+);
 
-it('can be instantiated using ::post()', function(){
-    $testClass = \Trunk\VtigerSDK\Http\VtigerRequest::post();
-    expect($testClass)->toBeInstanceOf(\Trunk\VtigerSDK\Http\VtigerRequest::class);
-    expect($testClass->getRequestType())->toBe('POST');
-});
+it(
+    'can be instantiated using ::post()',
+    function () {
+        $testClass = VtigerRequest::post();
+        expect($testClass)->toBeInstanceOf(VtigerRequest::class);
+        expect($testClass->getRequestType())->toBe('POST');
+    }
+);
 
-it('can get and set request paramters', function(){
-    $testClass = \Trunk\VtigerSDK\Http\VtigerRequest::get()->withParameter('testing','is the best');
-    expect($testClass->getParameters())->toBe(['testing'=>'is the best']);
-});
+it(
+    'can get and set request parameters',
+    function () {
+        $testClass = VtigerRequest::get()->withParameter('testing', 'is the best');
+        expect($testClass->getParameters())->toBe(['testing' => 'is the best']);
+    }
+);
 
-it('can get and set return type', function(){
-    $testClass = \Trunk\VtigerSDK\Http\VtigerRequest::get()->return(\Trunk\VtigerSDK\Http\VtigerResponse::class);
-    expect($testClass->getReturnType())->toBe(\Trunk\VtigerSDK\Http\VtigerResponse::class);
-});
+it(
+    'can get and set return type',
+    function () {
+        $testClass = VtigerRequest::get()->return(VtigerResponse::class);
+        expect($testClass->getReturnType())->toBe(VtigerResponse::class);
+    }
+);
 
